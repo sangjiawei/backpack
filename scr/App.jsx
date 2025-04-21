bbc716d5a9a775620b731ae4b78b9916f2f50ebf
曙光 <57454151+sangjiawei@users.noreply.github.com>
2025-04-21 15:16:29 +0800

    Add files via upload

---
+++ scr/App.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+
+function App() {
+  return (
+    <div className="dark:bg-[#111] min-h-screen text-white p-4">
+      <h1 className="text-2xl">Backpack 网格机器人</h1>
+    </div>
+  )
+}
+
+export default App
