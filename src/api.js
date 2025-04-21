884cb664138acb589ef911d7b7625382333f22ad
曙光 <57454151+sangjiawei@users.noreply.github.com>
2025-04-21 22:26:34 +0800

    Add files via upload

---
+++ src/api.js
@@ -0,0 +1,13 @@
+// src/api.js
+const API_BASE_URL = "http://43.160.201.61:8000/api";
+
+export async function login(username, password) {
+  const response = await fetch(`${API_BASE_URL}/login`, {
+    method: "POST",
+    headers: {
+      "Content-Type": "application/json"
+    },
+    body: JSON.stringify({ username, password })
+  });
+  return response.json();
+}
