467ec177f85db5dd1f6e7f0cb792c21e30491e82
曙光 <57454151+sangjiawei@users.noreply.github.com>
2025-04-23 15:29:07 +0800

    Update api.js

--- src/api.js
+++ src/api.js
@@ -1,5 +1,5 @@
 // src/api.js
-const API_BASE_URL = "http://api.shuguang.tech:8000/api";
+const API_BASE_URL = "https://api.shuguang.tech:8000/api";
 
 export async function login(username, password) {
   const response = await fetch(`${API_BASE_URL}/login`, {
